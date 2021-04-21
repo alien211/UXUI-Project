62d69c1a15c43795c3cb02fa7041a7427f76c4b5
NProlic <nikola.prolic@gmail.com>
2021-04-21 13:01:16 +0200

    Tones of changes
    Menu is fully functional, accordion is working again. The map is in the process of being made

--- js/js.js
+++ js/js.js
@@ -19,48 +19,6 @@ function openTab(evt, cityName) {
 document.getElementById("about").click();
 
 
-// Menu icon effect
-
-function myFunction(x) {
-    x.classList.toggle("change");
-}
-
-
-// Slideshow JS
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var j;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (j = 0; j < slides.length; j++) {
-        slides[j].style.display = "none";
-    }
-    for (j = 0; j < dots.length; j++) {
-        dots[j].className = dots[j].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
 
 // Accordion
 
@@ -84,3 +42,97 @@ for (k = 0; k < acc.length; k++) {
         }
     });
 }
+
+
+
+// Menu icon effect
+
+function iconTransform(x) {
+    x.classList.toggle("change");
+
+    // show the menus to the left
+    //document.getElementById(id).style.property = new style
+
+    //document.getElementById('about').style.left = "0px";
+    //document.getElementById('blog').style.left = "0px";
+    //document.getElementById('projects').style.left = "0px";
+    //document.getElementById('contact').style.left = "0px";
+}
+
+var popup = false;
+var buttons = document.getElementsByClassName("button");
+
+function leftMenu() {
+    if (!popup) {
+        // show the menus on the left
+        for (var i = 0; i < buttons.length; i++)
+            buttons[i].style.left = "0px";
+        //document.getElementById('about').style.left = "0px";
+        //document.getElementById('blog').style.left = "0px";
+        //document.getElementById('projects').style.left = "0px";
+        //document.getElementById('contact').style.left = "0px";
+    } else {
+        //hides the menu on the left 
+        for (var i = 0; i < buttons.length; i++)
+            buttons[i].style.left = null;
+        //document.getElementById('about').style.left = null;
+        //document.getElementById('blog').style.left = null;
+        //document.getElementById('projects').style.left = null;
+        //document.getElementById('contact').style.left = null;
+    }
+    popup = !popup;
+}
+
+
+
+// Slideshow JS
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var j, slides, dots;
+    slides = document.getElementsByClassName("mySlides");
+    dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (j = 0; j < slides.length; j++) {
+        slides[j].style.display = "none";
+    }
+    for (j = 0; j < dots.length; j++) {
+        dots[j].className = dots[j].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+//
+function Divs() {
+    var divs = $('.slideshow-container div'),
+        now = divs.filter(':visible'),
+        next = now.next().length ? now.next() : divs.first(),
+        speed = 1000;
+
+    now.fadeOut(speed);
+    next.fadeIn(speed);
+}
+
+$(function () {
+    setInterval(Divs, 1400);
+});
+
+
+
